3f1f085e783daa78457e9235515a192534f59798
dudz <dzvcs@proton.me>
2024-12-24 20:09:12 -0300

    feat: Implement job filtering based on form input - Added a new `filteredJobs` state to store the filtered job list - Updated `handleSubmitEvent` to filter jobs based on title, location, and contract type

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,6 +5,7 @@ import Card from './Card.tsx';
 
 function Main({ darkMode, onDarkModeChange }: { darkMode: boolean; onDarkModeChange: () => void }) {
 	const [jobs, setJobs] = useState<Job[]>([]);
+	const [filteredJobs, setFilteredJobs] = useState<Job[]>([]);
 
 	const [formData, setFormData] = useState({
 		title: '',
@@ -21,6 +22,7 @@ function Main({ darkMode, onDarkModeChange }: { darkMode: boolean; onDarkModeCha
 			const response = await fetch('/FakeData/data.json');
 			const jobData: Job[] = await response.json();
 			setJobs(jobData);
+			setFilteredJobs(jobData);
 			console.log(jobData);
 		} catch (e) {
 			console.log(e);
@@ -29,14 +31,23 @@ function Main({ darkMode, onDarkModeChange }: { darkMode: boolean; onDarkModeCha
 
 	const handleSubmitEvent = (e: React.FormEvent) => {
 		e.preventDefault();
-		console.log(formData.title, formData.location, formData.isFullTime);
+		const filteredJobs: Job[] = jobs.filter((job) => {
+			const matchTitle = job.position.toLowerCase().includes(formData.title.toLowerCase());
+			const matchLocation = job.location.toLowerCase().includes(formData.location.toLowerCase());
+			const matchFullTime = formData.isFullTime
+				? job.contract === 'Full Time'
+				: job.contract === 'Part Time';
+			return matchTitle && matchLocation && matchFullTime;
+		});
+
+		setFilteredJobs(filteredJobs);
 	};
 
 	return (
 		<main className="flex flex-col items-center justify-center">
 			<SearchBar onSubmit={handleSubmitEvent} formData={formData} setFormData={setFormData} />
 			<div className="flex flex-col gap-10 md:grid md:grid-cols-3 md:gap-24">
-				{jobs.map((job) => (
+				{filteredJobs.map((job) => (
 					<Card key={job.id} {...job} darkMode={darkMode} onDarkModeChange={onDarkModeChange} />
 				))}
 			</div>
